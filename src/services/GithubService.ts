08a6f3e9c9f0b4277b0b4d00208a43ab3024e848
Jose Pinduisaca <jos.frns@gmail.com>
2026-06-30 14:59:01 -0500

    examen

--- src/services/GithubService.ts
+++ src/services/GithubService.ts
@@ -14,15 +14,15 @@ const apiClient = axios.create({
     }
 });
 
-export const fetchRepositories = async() : Promise<Repository[]> => {
+export const fetchRepositories = async (): Promise<Repository[]> => {
     try {
-        const response = await apiClient.get('user/repos', {
+        const response = await apiClient.get(`/user/repos`, {
             params: {
                 per_page: 100,
                 sort: 'created',
                 direction: 'desc',
                 affiliation: 'owner',
-                t: Date.now() // Evitar cache
+                t: Date.now()
             }
         });
         if (response.status !== 200) {
@@ -32,11 +32,11 @@ export const fetchRepositories = async() : Promise<Repository[]> => {
     } catch (error) {
         throw new Error(`${(error as Error).message}`);
     }
-}
+};
 
-export const createRepository = async(repository: RepositoryPayload) : Promise<Repository | null> => {
+export const createRepository = async (repository: RepositoryPayload): Promise<Repository | null> => {
     try {
-        const response = await apiClient.post('user/repos', repository);
+        const response = await apiClient.post(`/user/repos`, repository);
         if (response.status !== 201) {
             throw new Error(`${response.statusText}`);
         }
@@ -44,11 +44,11 @@ export const createRepository = async(repository: RepositoryPayload) : Promise<R
     } catch (error) {
         throw new Error(`${(error as Error).message}`);
     }
-}
+};
 
-export const fetchUserInfo = async() : Promise<GithubUser | null> => {
+export const fetchUserInfo = async (): Promise<GithubUser | null> => {
     try {
-        const response = await apiClient.get('user');
+        const response = await apiClient.get(`/user`);
         if (response.status !== 200) {
             throw new Error(`${response.statusText}`);
         }
@@ -56,4 +56,33 @@ export const fetchUserInfo = async() : Promise<GithubUser | null> => {
     } catch (error) {
         throw new Error(`${(error as Error).message}`);
     }
-}
+};
+
+
+
+export const updateRepository = async (owner: string, repo: string, repositoryData: Partial<RepositoryPayload>): Promise<Repository | null> => {
+    try {
+       
+        const response = await apiClient.patch(`/repos/${owner}/${repo}`, repositoryData);
+        if (response.status !== 200) {
+            throw new Error(`${response.statusText}`);
+        }
+        return response.data;
+    } catch (error) {
+        throw new Error(`${(error as Error).message}`);
+    }
+};
+
+export const deleteRepository = async (owner: string, repo: string): Promise<boolean> => {
+    try {
+        // DELETE y la ruta incluye el dueño y el nombre del repo
+        const response = await apiClient.delete(`/repos/${owner}/${repo}`);
+       
+        if (response.status !== 204) {
+            throw new Error(`${response.statusText}`);
+        }
+        return true;
+    } catch (error) {
+        throw new Error(`${(error as Error).message}`);
+    }
+};
